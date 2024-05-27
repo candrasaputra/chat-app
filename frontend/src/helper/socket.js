4292eaaf024836fb840a4f6e19d7332fd6d21b4d
Candra Saputra <candrasaputra@live.com>
2024-05-27 19:01:26 +0700

    fix: docker compose access

--- frontend/src/helper/socket.js
+++ frontend/src/helper/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socketio = (token) => io(process.env.REACT_APP_BACKEND_SOCKET_URL, {
+export const socketio = (token) => io(process.env.REACT_APP_BACKEND_SOCKET_URL || 'ws://localhost:8081', {
         extraHeaders: {
             authorization: `bearer ${token}`
         },
